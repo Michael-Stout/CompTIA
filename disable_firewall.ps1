13e313bef4a25277aef0a82929d5019921f5bbd9
Michael Stout <114250811+Michael-Stout@users.noreply.github.com>
2024-03-18 10:01:06 +0000

    Delete disable_firewall.ps1

--- disable_firewall.ps1
+++
@@ -1,5 +0,0 @@
-# Turn off Windows Defender Firewall for all profiles
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-
-# Output to confirm action
-Write-Output "Windows Defender Firewall has been turned off for all profiles."
