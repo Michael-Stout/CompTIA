f0af26faad64ba16b57ce4f40147c4aacd923ade
Michael Stout <114250811+Michael-Stout@users.noreply.github.com>
2024-03-18 09:10:11 +0000

    Update and rename vm_download.ps1 to client_setup.ps1

--- client_setup.ps1
+++ client_setup.ps1
@@ -1,3 +1,9 @@
+# Turn off Windows Defender Firewall for all profiles
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+# Output to confirm action
+Write-Output "Windows Defender Firewall has been turned off for all profiles."
+
 # Define the source and destination paths
 $sourcePath = '\\instructor\share'
 $destinationPath = 'D:'
